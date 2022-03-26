8cba576338cd564ca0bba10b524af339ed48a7a9
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-03-26 09:00:48 +0530

    Added: function day of week

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         JavaJUnitPrograms javaJUnitPrograms = new JavaJUnitPrograms();
-        javaJUnitPrograms.vendingMachine(999);
+//        javaJUnitPrograms.vendingMachine(999);
+
+        String weekDay=javaJUnitPrograms.dayOfWeek(19,3,2022);
+        System.out.println(weekDay);
     }
 }
