0b9579fca32267afb4390f223a34091f7111d964
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-03-27 14:23:01 +0530

    Added:function monthly salary

--- src/JavaJUnitPrograms.java
+++ src/JavaJUnitPrograms.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -11,43 +12,51 @@ public class JavaJUnitPrograms {
             if (amount >= notes[len]) {
                 note[counter++] = notes[len];
                 amount -= notes[len];
-                if(amount>=notes[len])
+                if (amount >= notes[len])
                     continue;
                 len--;
-            }else
+            } else
                 len--;
         }
         System.out.println(Arrays.toString(note));
     }
 
-    public String dayOfWeek(int d,int m,int y){
-        String[] weekDays={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-        int y0 = y- (14 - m) / 12;
-        int x = y0 + y0 /4 - y0 /100 + y0 /400;
+    public String dayOfWeek(int d, int m, int y) {
+        String[] weekDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
         int m0 = m + 12 * ((14 - m) / 12) - 2;
-        int d0 = (d + x + (31*m0) / 12) % 7;
+        int d0 = (d + x + (31 * m0) / 12) % 7;
         return weekDays[d0];
     }
 
-    public static String convertTemperature(){
+    public static String convertTemperature() {
         Scanner in = new Scanner(System.in);
         System.out.println("1. Convert Fahrenheit to Celsius\n2.Convert Celsius to Fahrenheit");
         System.out.println("Enter Choice: ");
-        int ch=in.nextInt();
-        int input=0;
-        double temp=0;
-        switch (ch){
+        int ch = in.nextInt();
+        int input = 0;
+        double temp = 0;
+        switch (ch) {
             case 1:
                 System.out.println("Enter temperature in Fahrenheit(>32): ");
-                input=in.nextInt();
-                temp=(input - 32) * (5.0/9.0);
+                input = in.nextInt();
+                temp = (input - 32) * (5.0 / 9.0);
                 break;
             case 2:
                 System.out.println("Enter temperature in Celsius: ");
-                input=in.nextInt();
-                temp=(input * (9.0/5.0)) + 32;
+                input = in.nextInt();
+                temp = (input * (9.0 / 5.0)) + 32;
                 break;
         }
         return String.format("%.2f", temp);
     }
+
+    public static double calculateMonthlyPayment(double p, float r, int y) {
+        int n = 12 * y;
+        double rate = r / (12 * 100);
+        double payment = (p * rate) / (1 - (Math.pow((1 + rate), -n)));
+        return Math.round(payment);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ public class Main {
 //        String weekDay=javaJUnitPrograms.dayOfWeek(19,3,2022);
 //        System.out.println(weekDay);
 
-        String temp=JavaJUnitPrograms.convertTemperature();
-        System.out.println(temp);
+//        String temp=JavaJUnitPrograms.convertTemperature();
+//        System.out.println(temp);
+
+        double payment=JavaJUnitPrograms.calculateMonthlyPayment(Double.parseDouble(args[0]),Float.parseFloat(args[1]),Integer.parseInt(args[2]));
+        System.out.println(payment);
+
     }
 }
